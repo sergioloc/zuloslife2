ad17955e8d3004c05aa462870799e0d688ad2775
Sergio <sergioloc2@gmail.com>
2022-02-24 15:44:21 +0100

    Llamilio look at target

--- Assets/Scripts/LlamilioController.cs
+++ Assets/Scripts/LlamilioController.cs
@@ -6,8 +6,12 @@ public class LlamilioController : MonoBehaviour {
 
     public float speed;
     public ParticleSystem fireParticles;
+    public GameObject target;
 
     private bool run;
+    private float distanceToTarget;
+    private bool lookRight = true;
+    private float limit = 2.3f;
 
     void Start() {
        
@@ -17,31 +21,54 @@ public class LlamilioController : MonoBehaviour {
         if (run) {
             transform.Translate(Vector2.right * (speed/10) * Time.deltaTime);
         }
+        else {
+            distanceToTarget = target.transform.position.x - transform.position.x;
+            LookAtTarget();
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ground") {
-            run = true;
+            //run = true;
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.tag == "Enemy") {
-            StartCoroutine(StopRunning());
+        if (collision.gameObject.tag == "Kasper") {
+            //fireParticles.Play();
+            run = false;
         }
     }
 
     void OnTriggerExit2D(Collider2D collision) {
-        if (collision.gameObject.tag == "Enemy") {
+        if (collision.gameObject.tag == "Kasper") {
+            //fireParticles.Stop();
             run = true;
-            fireParticles.Stop();
+            if (!lookRight) {
+                LookRight();
+            }
         }
     }
-    
-    IEnumerator StopRunning() {
-        yield return new WaitForSeconds(1f);
-        run = false;
-        fireParticles.Play();
+
+    private void LookAtTarget() {
+        if (distanceToTarget > 0 && !lookRight) {
+            LookRight();
+        }
+        else if (distanceToTarget < 0 && lookRight) {
+            LookLeft();
+        }
+    }
+
+    private void LookRight() {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        lookRight = true;
+        limit = -limit;
+    }
+
+    private void LookLeft() {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        lookRight = false;
+        limit = -limit;
     }
 
 }
