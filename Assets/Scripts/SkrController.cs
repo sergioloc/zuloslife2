09d4d7b5f9614213e326be05d4dfaccc7417d071
Sergio <sergioloc2@gmail.com>
2022-02-27 14:47:19 +0100

    Start shockwave at the center the screen

--- Assets/Scripts/SkrController.cs
+++ Assets/Scripts/SkrController.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class SkrController : MonoBehaviour {
 
-    public UnityEngine.Rendering.Universal.UniversalAdditionalCameraData cam;
-    public int newIndex;
+    public float speed;
+    public float sockwaveTime;
+    private UnityEngine.Rendering.Universal.UniversalAdditionalCameraData camData;
+    private bool run = false;
     
     void Start() {
-        StartCoroutine(StopRunning());
+        GameObject[] camList = GameObject.FindGameObjectsWithTag("MainCamera");
+        camData = camList[0].GetComponent<UnityEngine.Rendering.Universal.UniversalAdditionalCameraData>();
     }
 
-    
-    IEnumerator StopRunning() {
-        yield return new WaitForSeconds(2f);
-        cam.SetRenderer(newIndex);
+    void FixedUpdate() {
+        if (run) {
+            transform.Translate(Vector2.right * (speed/10) * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        // Start running whem character touches ground
+        if (collision.gameObject.tag == "Ground" && !run) {
+            run = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Center") {
+            StartCoroutine(Shockwave());
+        }
+    }
+
+    IEnumerator Shockwave() {
+        yield return new WaitForSeconds(1f);
+        run = false;
+        camData.SetRenderer(1);
+        yield return new WaitForSeconds(sockwaveTime);
+        camData.SetRenderer(0);
     }
 
 }
