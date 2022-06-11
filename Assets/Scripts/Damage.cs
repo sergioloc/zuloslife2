149f1bd745397a972dc1f4471fd79a1ae6d2e4a9
Sergio <sergioloc2@gmail.com>
2022-06-11 17:00:43 +0200

    Fix prefab layers

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    [SerializeField] private float health;
+    [SerializeField] private bool isEnemy;
+
+    [Header("Character")]
+    [SerializeField] private float fire; // Llamilio
+    [SerializeField] private float leave; // GreenLife
+    [SerializeField] private float roller; // Stere
+    [SerializeField] private float onion; // Cepa
+    [SerializeField] private float shockwave; // Skr
+
+    [Header("Enemy")]
+    [SerializeField] private float mucus; // Moquino
+    [SerializeField] private float stone; // Muro
+    [SerializeField] private float kasper; // Kasper
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (isEnemy) {
+            switch(collision.gameObject.tag) {
+                case "Fire":
+                health -= fire;
+                break;
+
+                case "Leave":
+                health -= leave;
+                break;
+                
+                case "Roller":
+                health -= roller;
+                break;
+
+                case "Onion":
+                health -= onion;
+                break;
+
+                case "Shockwave":
+                health -= shockwave;
+                break;
+            }
+        }
+        else {
+            switch(collision.gameObject.tag) {
+                case "Mucus":
+                health -= mucus;
+                break;
+
+                case "Stone":
+                health -= stone;
+                break;
+
+                case "Kasper":
+                health -= kasper;
+                break;
+            }
+        }
+
+        if (health < 0)
+            Destroy(gameObject);
+
+        //collision.gameObject.GetComponent<Animator>().SetTrigger("Absorb");
+    }
+
+
+}
