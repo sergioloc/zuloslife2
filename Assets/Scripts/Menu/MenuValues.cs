1d6edf1c7fb8e2fdf8cf8710804aa0259d1e70e9
Sergio <sergioloc2@gmail.com>
2022-06-30 11:52:18 +0200

    New menu scene

---
+++ Assets/Scripts/Menu/MenuValues.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MenuValues {
+
+    public static float position = 7f;
+    public static bool moveRight = false;
+    public static bool muteMusic = GetMuteValue();
+
+    private static bool GetMuteValue() {
+        if (PlayerPrefs.GetInt("Mute") == 0)
+            return false;
+        else
+            return true;
+    }
+
+}
---
+++ Assets/Scripts/Menu/Skyline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skyline : MonoBehaviour {
+
+    public int speed;
+    
+    void Start() {
+        transform.position = new Vector2(MenuValues.position, 0f);
+    }
+
+    void FixedUpdate() {
+        if (MenuValues.moveRight) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (transform.position.x > 7)
+                MenuValues.moveRight = false;
+        }
+        else {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (transform.position.x < -7)
+                MenuValues.moveRight = true;
+        }
+        MenuValues.position = transform.position.x;
+    }
+
+}
