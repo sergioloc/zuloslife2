e61cd96866ab074f0f009a4da254a6e2791ea493
Sergio <sergioloc2@gmail.com>
2022-07-13 16:56:24 +0200

    New level timer

---
+++ Assets/Scripts/Level3/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField] private float time = 0;
+    private float currentTime;
+    private Image bar;
+
+    void Start() {
+        currentTime = time;
+        bar = GetComponent<Image>();
+        bar.fillAmount = 1f;
+    }
+
+    void Update() {
+        if (currentTime > 0) {
+            currentTime -= Time.deltaTime;
+            bar.fillAmount = currentTime / time;
+        }
+        else
+            Destroy(gameObject);
+    }
+
+}
